06ebbdbd25310a58762c8b83de5f2d3af98481b3
Wandago <122892133+Ligawa2547@users.noreply.github.com>
2026-03-28 00:19:15 +0300

    Update checkbox component to use Radix UI and Tailwind CSS

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,22 +1,13 @@
-import * as React from 'react';
-import { styled } from '@stitches/react';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { CheckBox } from '@radix-ui/react-checkbox';
+import { cn } from '@/lib/utils';
+import { forwardRef } from 'react';
 
-const StyledCheckbox = styled(CheckboxPrimitive.Checkbox, {
-  width: 20,
-  height: 20,
-  backgroundColor: 'white',
-  border: '2px solid gray',
-  borderRadius: '4px',
-  outline: 'none',
-  '&[data-state="checked"]': {
-    backgroundColor: 'blue',
-    borderColor: 'blue',
-  },
-});
-
-export const Checkbox = React.forwardRef((props, forwardedRef) => (
-  <StyledCheckbox {...props} ref={forwardedRef} />
+const Checkbox = forwardRef(({ className, ...props }, ref) => (
+  <CheckBox
+    ref={ref}
+    className={cn('h-4 w-4 rounded ring-offset-gray-100 focus:ring-2 checked:ring-4', className)}
+    {...props}
+  />
 ));
 
 Checkbox.displayName = 'Checkbox';
